201063692349eda79a4ee9e41d3317281f1ba282
ameen-ulhaq <dev.ameenulhaq@gmail.com>
2022-03-26 02:16:11 +0500

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium autem necessitatibus, ab officia corrupti aliquid distinctio facilis quidem quas illum delectus, magnam inventore velit aperiam ad aut! Quam, commodi ratione!</p>
+    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium autem necessitatibus, ab officia corrupti aliquid distinctio facilis quidem quas illum delectus, magnam inventore velit aperiam ad aut! Quam, commodi ratione!</p>
+    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium autem necessitatibus, ab officia corrupti aliquid distinctio facilis quidem quas illum delectus, magnam inventore velit aperiam ad aut! Quam, commodi ratione!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium autem necessitatibus, ab officia corrupti aliquid distinctio facilis quidem quas illum delectus, magnam inventore velit aperiam ad aut! Quam, commodi ratione!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium autem necessitatibus, ab officia corrupti aliquid distinctio facilis quidem quas illum delectus, magnam inventore velit aperiam ad aut! Quam, commodi ratione!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium autem necessitatibus, ab officia corrupti aliquid distinctio facilis quidem quas illum delectus, magnam inventore velit aperiam ad aut! Quam, commodi ratione!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium autem necessitatibus, ab officia corrupti aliquid distinctio facilis quidem quas illum delectus, magnam inventore velit aperiam ad aut! Quam, commodi ratione!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium autem necessitatibus, ab officia corrupti aliquid distinctio facilis quidem quas illum delectus, magnam inventore velit aperiam ad aut! Quam, commodi ratione!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium autem necessitatibus, ab officia corrupti aliquid distinctio facilis quidem quas illum delectus, magnam inventore velit aperiam ad aut! Quam, commodi ratione!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium autem necessitatibus, ab officia corrupti aliquid distinctio facilis quidem quas illum delectus, magnam inventore velit aperiam ad aut! Quam, commodi ratione!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium autem necessitatibus, ab officia corrupti aliquid distinctio facilis quidem quas illum delectus, magnam inventore velit aperiam ad aut! Quam, commodi ratione!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium autem necessitatibus, ab officia corrupti aliquid distinctio facilis quidem quas illum delectus, magnam inventore velit aperiam ad aut! Quam, commodi ratione!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium autem necessitatibus, ab officia corrupti aliquid distinctio facilis quidem quas illum delectus, magnam inventore velit aperiam ad aut! Quam, commodi ratione!</p>
+  </div>
+</template>
